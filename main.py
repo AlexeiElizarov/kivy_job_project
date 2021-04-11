b8cd0fe43f990c6be71d36240c556215ecabe61b
AlexeiElizarov <alexeiasd2@gmail.com>
2021-04-11 20:37:43 +0400

    два экрана

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class MyApp(App):
-    def build(self):
-        return Button(text='hellow')
+class MainMenu():
+    """
+        Главный экран приложения
+    """
+    pass
 
+
+class CreateOrder():
+    """
+        Экран с формой заплнения данных наряда
+    """
+    pass
+
+
+class JobApp(App):
+
+    pass
 
 if __name__ == '__main__':
-    MyApp().run()
+    JobApp().run()
